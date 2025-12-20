aa6c88e8a39baec65fd3d6b659b136cb2900f4a1
c-Balasubramanian <balainfilabs@gmail.com>
2025-12-20 13:43:15 +0530

    new

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Send text message
 export async function sendMessage(message) {
-  const res = await fetch(`${API_BASE_URL}/chat/`, {
+  const res = await fetch(`${API_BASE_URL}/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,7 +19,7 @@ export async function sendMessage(message) {
 
 // Send image file
 export async function sendImage(formData) {
-  const res = await fetch(`${API_BASE_URL}/upload-image/`, {
+  const res = await fetch(`${API_BASE_URL}/upload-image`, {
     method: "POST",
     body: formData, // no headers
   });
